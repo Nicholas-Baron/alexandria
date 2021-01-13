11e0b5e23722d315b2963d1844bc2a4285640805
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2021-01-12 20:31:16 -0800

    Added test for alx::exchange

---
+++ test/utility.cpp
@@ -0,0 +1,30 @@
+#include "utility.h"
+
+#include <boost_ut/ut.hpp>
+
+int main() {
+    using namespace boost::ut;
+
+    "exchange moves a value while replacing it"_test = [] {
+        struct test_t final {
+            int x;
+
+            test_t(int val)
+                : x{val} {}
+
+            test_t(const test_t &) = delete;
+            test_t(test_t &&) noexcept = default;
+
+            test_t & operator=(const test_t &) = delete;
+            test_t & operator=(test_t &&) noexcept = default;
+
+            ~test_t() noexcept = default;
+        };
+
+        test_t src{1};
+        test_t dest = alx::exchange(src, 2);
+        expect(eq(src.x, 2));
+        expect(eq(dest.x, 1));
+    };
+}
+
