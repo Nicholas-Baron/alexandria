ef76edd8142e48f404dcf486fb7e499558b3fa92
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2021-01-12 20:43:39 -0800

    Removed commented out function

--- src/global_mem.cpp
+++ src/global_mem.cpp
@@ -8,16 +8,6 @@
         return ptr;
 }
 
-/*
-[[nodiscard]] void * operator new(size_t size, std::align_val_t al) {
-    void * ptr = aligned_alloc(static_cast<size_t>(al), size);
-    if (ptr == nullptr) {
-        exit(-256);
-    } else
-        return ptr;
-}
-*/
-
 [[nodiscard]] void * operator new[](size_t size) {
     void * ptr = malloc(size);
     if (ptr == nullptr) {
