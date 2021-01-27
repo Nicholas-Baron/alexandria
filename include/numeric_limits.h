6205263cf7bc14e9b77a1bea36948c203ce1cbf5
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2021-01-27 12:53:04 -0800

    Added all numeric types

--- include/numeric_limits.h
+++ include/numeric_limits.h
@@ -22,24 +22,31 @@ namespace alx {
     }
 
     NUM_LIM_INST(bool, false, true, true);
+
     NUM_LIM_INST(char, CHAR_MIN < 0, true, true);
     NUM_LIM_INST(signed char, true, true, true);
-    NUM_LIM_INST(bool, false, true, true);
-    NUM_LIM_INST(bool, false, true, true);
-    NUM_LIM_INST(bool, false, true, true);
-    NUM_LIM_INST(bool, false, true, true);
-    NUM_LIM_INST(bool, false, true, true);
-    NUM_LIM_INST(bool, false, true, true);
-    NUM_LIM_INST(bool, false, true, true);
-    NUM_LIM_INST(bool, false, true, true);
-    NUM_LIM_INST(bool, false, true, true);
-    NUM_LIM_INST(bool, false, true, true);
-    NUM_LIM_INST(bool, false, true, true);
-    NUM_LIM_INST(bool, false, true, true);
-    NUM_LIM_INST(bool, false, true, true);
-    NUM_LIM_INST(bool, false, true, true);
-    NUM_LIM_INST(bool, false, true, true);
-    NUM_LIM_INST(bool, false, true, true);
+    NUM_LIM_INST(unsigned char, false, true, true);
+
+    NUM_LIM_INST(short, false, true, true);
+    NUM_LIM_INST(unsigned short, false, true, true);
+
+    NUM_LIM_INST(int, false, true, true);
+    NUM_LIM_INST(unsigned, false, true, true);
+
+    NUM_LIM_INST(long, false, true, true);
+    NUM_LIM_INST(unsigned long, false, true, true);
+
+    NUM_LIM_INST(long long, false, true, true);
+    NUM_LIM_INST(unsigned long long, false, true, true);
+
+    NUM_LIM_INST(char8_t, false, true, true);
+    NUM_LIM_INST(char16_t, false, true, true);
+    NUM_LIM_INST(char32_t, false, true, true);
+    NUM_LIM_INST(wchar_t, false, true, true);
+
+    NUM_LIM_INST(float, true, false, false);
+    NUM_LIM_INST(double, true, false, false);
+    NUM_LIM_INST(long double, true, false, false);
 
 #undef NUM_LIM_INST
 } // namespace alx
