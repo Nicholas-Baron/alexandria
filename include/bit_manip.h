1afd785fbd07b7b251c805a71fc62204ab2b30ff
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2021-01-27 00:04:50 -0800

    Added an is_power_of_2 function

--- include/bit_manip.h
+++ include/bit_manip.h
@@ -1,6 +1,8 @@
 #ifndef BIT_MANIP_H
 #define BIT_MANIP_H
 
+#include "concepts.h"
+
 namespace alx {
 
     template<typename T>
@@ -9,6 +11,20 @@ namespace alx {
         for (; x != 0; count++) x &= x - 1;
         return count;
     }
+
+    // clang-format off
+    template<unsigned_integral T>
+    requires(not same_as<T, bool>
+            and not same_as<T, char>
+            and not same_as<T, char8_t>
+            and not same_as<T, char16_t>
+            and not same_as<T, char32_t>
+            and not same_as<T, wchar_t>)
+    constexpr bool is_power_of_2(T x) noexcept {
+        return popcount(x) == 1;
+    }
+    // clang-format on
+
 } // namespace alx
 
 #endif
--- test/bit_manip.cpp
+++ test/bit_manip.cpp
@@ -13,4 +13,17 @@ int main() {
 
         expect(eq(popcount(15), 4));
     };
+
+    "is_power_of_2 return whether a number is a power of 2"_test = [] {
+        using alx::is_power_of_2;
+
+        expect(not is_power_of_2(0u));
+        expect(is_power_of_2(1u));
+
+        for (int i = 1; i < 4; i++) expect(is_power_of_2(1u << i));
+
+        expect(not is_power_of_2(5u));
+        expect(not is_power_of_2(17u));
+        expect(not is_power_of_2(25u));
+    };
 }
